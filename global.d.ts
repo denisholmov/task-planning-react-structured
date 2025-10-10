bf2a9ad63a7c9161b99b9781d48bb5a9e0fc783e
denis <denisskholmov@gmail.com>
2025-10-10 11:52:33 +0300

    добавил тип для svg иконок в настройки global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -6,3 +6,9 @@ declare module "*.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.svg?react" {
+  import { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGSVGElement>>;
+  export default content;
+}
