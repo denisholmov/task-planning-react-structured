ed49cb3fc27ad7dd62be6e1a6f0e44051e10534f
denis <denisskholmov@gmail.com>
2025-10-12 11:58:32 +0300

    Предварительно подготовил запрос на получение задач в компоненте board

--- src/components/features/Main/index.tsx
+++ src/components/features/Main/index.tsx
@@ -3,5 +3,6 @@ import type { MainProps } from "./types/interface";
 import styles from "./styles/Main.module.css";
 
 export const Main = ({ children }: MainProps) => {
-  return <main className={styles.main}>{children}</main>;
+  return <main className={styles.main}>
+		{children}</main>;
 };
--- src/components/pages-features/Board/index.tsx
+++ src/components/pages-features/Board/index.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+
+import type { Task } from "./types/interface";
+
 export const Board = () => {
+  const [tasks, setTasks] = React.useState<Task[] | null>(null);
+
+  fetch("https://68eb647e76b3362414cd8cc7.mockapi.io/tasks")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => alert(error)); // подготовил запрос на бэкенд
+
   return <div className="board">Board</div>;
 };
